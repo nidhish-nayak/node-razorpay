fcd4cdd373f03e7087ab3bfdb9e6155b12a83394
Nidhish-D-Nayak <nidhibelthangady@gmail.com>
2023-06-30 00:33:28 +0530

    Basic server setup complete

---
+++ server.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+const storeItems = new Map([
+	[1, { priceInCents: 10000, name: "Nidhish" }],
+	[2, { priceInCents: 20000, name: "Nishanth" }],
+]);
+
+app.listen(3000);
