50fdc5807a829add2303b3ab987ab7bd730a09cb
Nidhish-D-Nayak <nidhibelthangady@gmail.com>
2023-07-02 00:10:15 +0530

    Updated index.js payment code

--- index.js
+++ index.js
@@ -11,6 +11,30 @@ app.use(bodyParser.json());
 
 app.use(cors());
 
+app.post("/payment", cors(), async (req, res) => {
+	let { amount, id } = req.body;
+	try {
+		const payment = await stripe.paymentIntents.create({
+			amount,
+			currency: "inr",
+			description: "Fabric clothing payment testing",
+			payment_method: id,
+			confirm: true,
+		});
+		console.log("Payment: ", payment);
+		res.json({
+			message: "Payment successful",
+			success: true,
+		});
+	} catch (error) {
+		console.log(error);
+		res.json({
+			message: "Payment failed",
+			success: false,
+		});
+	}
+});
+
 app.listen(process.env.PORT || 4000, () => {
 	console.log("Listening to port 4000!");
 });
