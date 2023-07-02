bb765c01a6237ba09043ef416943f1e03ca23bbe
Nidhish-D-Nayak <nidhibelthangady@gmail.com>
2023-07-02 22:49:55 +0530

    Updated server.js - price added dynamic value

--- server.js
+++ server.js
@@ -11,55 +11,17 @@ const instance = new Razorpay({
 	key_secret: process.env.RAZORPAY_SECRET,
 });
 
-// API for creating orders using Razorpay SDK
-app.get("/order", (req, res) => {
+app.get("/orders", async (req, res) => {
+	const { price } = req.query;
+
 	try {
-		const options = {
-			amount: 10 * 100, // amount == Rs 10
+		const order = await instance.orders.create({
+			amount: price * 100,
 			currency: "INR",
 			receipt: "receipt#1",
-			payment_capture: 1,
-			// 1 for automatic capture // 0 for manual capture
-		};
-		instance.orders.create(options, async function (err, order) {
-			if (err) {
-				return res.status(500).json({
-					message: "Something Went Wrong",
-				});
-			}
-			return res.status(200).json(order);
 		});
-	} catch (err) {
-		return res.status(500).json({
-			message: "Something Went Wrong",
-		});
-	}
-});
 
-//API the captures the payment
-app.post("/capture/:paymentId", (req, res) => {
-	try {
-		return request(
-			{
-				method: "POST",
-				url: `https://${process.env.RAZORPAY_KEY_ID}:${process.env.RAZORPAY_SECRET}@api.razorpay.com/v1/payments/${req.params.paymentId}/capture`,
-				form: {
-					amount: 10 * 100, // amount == Rs 10 // Same As Order amount
-					currency: "INR",
-				},
-			},
-			async function (err, response, body) {
-				if (err) {
-					return res.status(500).json({
-						message: "Something Went Wrong",
-					});
-				}
-				console.log("Status:", response.statusCode);
-				console.log("Headers:", JSON.stringify(response.headers));
-				console.log("Response:", body);
-				return res.status(200).json(body);
-			}
-		);
+		return res.status(200).json(order);
 	} catch (err) {
 		return res.status(500).json({
 			message: "Something Went Wrong",
