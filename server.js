0a3dba2bcf6b08be41274380af2c55205afb09a1
Nidhish-D-Nayak <nidhibelthangady@gmail.com>
2023-07-02 23:35:59 +0530

    Added error value return on catch - get(/orders) async error

--- server.js
+++ server.js
@@ -4,9 +4,11 @@ const cors = require("cors");
 const app = express();
 require("dotenv").config();
 
+app.use(express.json());
 app.use(cors());
 
-const instance = new Razorpay({
+// The Razorpay instance is created outside the /orders route handler, and the instance is reused for each request.
+const razorpayInstance = new Razorpay({
 	key_id: process.env.RAZORPAY_KEY_ID,
 	key_secret: process.env.RAZORPAY_SECRET,
 });
@@ -15,16 +17,16 @@ app.get("/orders", async (req, res) => {
 	const { price } = req.query;
 
 	try {
-		const order = await instance.orders.create({
+		const order = await razorpayInstance.orders.create({
 			amount: price * 100,
 			currency: "INR",
 			receipt: "receipt#1",
 		});
-
 		return res.status(200).json(order);
-	} catch (err) {
+	} catch (error) {
 		return res.status(500).json({
 			message: "Something Went Wrong",
+			errorValue: error,
 		});
 	}
 });
