44d62a3d0b2ff97cb23e44bc922d18be025dcdeb
Nidhish-D-Nayak <nidhibelthangady@gmail.com>
2023-07-03 14:11:46 +0530

    Added cors

--- server.js
+++ server.js
@@ -9,7 +9,16 @@ const app = express();
 app.use(express.json());
 
 // CORS for production
-app.use(cors());
+app.use(
+	cors({
+		origin: [
+			"https://www.fabric-clothing.netlify.app",
+			"https://www.fabric-clothing.netlify.app/checkout",
+		],
+		methods: ["GET", "POST"],
+		allowedHeaders: ["Content-Type", "X-Requested-With"],
+	})
+);
 
 // Order APIs
 app.use("/orders", ordersRoute);
