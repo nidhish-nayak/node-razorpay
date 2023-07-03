cc031fda3e0367115a3a68e45f2675a48b80db1e
Nidhish-D-Nayak <nidhibelthangady@gmail.com>
2023-07-03 13:41:44 +0530

    added checkout endpoint to origin

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(
 	cors({
 		origin: [
-			"http://localhost:3000",
+			"http://localhost:3000/checkout",
 			"https://www.fabric-clothing.netlify.app",
 		],
 		methods: ["GET", "POST"],
